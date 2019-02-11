fa27ea1c1840e57c82910270e45777556647435b
johnsecules <john.secules@gmail.com>
2019-02-10 20:59:52 -0500

    Added the aws-php-sdk dependency to the composer file added the dynamodb connection stuff to setup.php

--- setup.php
+++ setup.php
@@ -1,4 +1,14 @@
 <?php
+require 'vendor/autoload.php';
+date_default_timezone_set('America/New_York');
+
+use Aws\DynamoDb\DynamoDbClient;
+$client = new DynamoDbClient([
+    'profile' => 'default',
+    'region'  => 'us-east-1',
+    'version' => 'latest'
+]);
+
 // put full path to Smarty.class.php
 require('vendor/smarty/smarty/libs/Smarty.class.php');
 $smarty = new Smarty();
